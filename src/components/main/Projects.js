e1e8f4b17c3eb8cab70fd8d2888c002eda59741d
Pabla23 <dilraj_pabla@hotmail.com>
2023-06-29 23:51:54 -0700

    basic mobile/desktop site structure complete

--- src/components/main/Projects.js
+++ src/components/main/Projects.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { restBase } from "../../globals/globals";
 import { featuredImage } from "../../globals/globals";
 import { Link } from "react-router-dom";
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 function Projects({restData, isLoaded}) {
 
@@ -23,6 +25,11 @@ function Projects({restData, isLoaded}) {
     fetchData()
   }, [restPath])
 
+  //reverse the order of the array
+  const restDataAdjusted = restDataCPT.slice().reverse();
+
+  const isMobile = window.innerWidth <= 700;
+
   return (
     isLoaded ?
       <section className="projects" id="projects" data-aos="fade-zoom-in" data-aos-easing="ease" data-aos-delay="200" data-aos-offset="0">
@@ -30,24 +37,37 @@ function Projects({restData, isLoaded}) {
           <h2>{restData.acf['projects_header']}</h2>
           <p>{restData.acf['projects_text']}</p>
 
-          <div className="projects_grid">
-
-            {isLoadedCPT ? restDataCPT.map((item, index) => {
-
-              return (
+          {isMobile ? (
+            <div className="projects_grid">
+              <Carousel showThumbs={false} infiniteLoop={true}>
+                {isLoadedCPT ? restDataAdjusted.map((item, index) => (
+                  <div className={`project p${item.id}`} key={index}>
+                    <Link to={`/project/${item.slug}`}>
+                      <figure className="featured_image" dangerouslySetInnerHTML={featuredImage(item._embedded['wp:featuredmedia'][0])}></figure>
+                      <div className="project_info">
+                        <h3>{item.title.rendered}</h3>
+                        <div dangerouslySetInnerHTML={{ __html: item.excerpt.rendered }}></div>
+                      </div>
+                    </Link>
+                  </div>
+                )) : null}
+              </Carousel>
+            </div>
+          ) : (
+            <div className="projects_grid">
+              {isLoadedCPT ? restDataAdjusted.map((item, index) => (
                 <div className={`project p${item.id}`} key={index}>
                   <Link to={`/project/${item.slug}`}>
                     <figure className="featured_image" dangerouslySetInnerHTML={featuredImage(item._embedded['wp:featuredmedia'][0])}></figure>
                     <div className="project_info">
                       <h3>{item.title.rendered}</h3>
-                      <div dangerouslySetInnerHTML={{__html: item.excerpt.rendered}}></div>
+                      <div dangerouslySetInnerHTML={{ __html: item.excerpt.rendered }}></div>
                     </div>
                   </Link>
                 </div>
-              )
-            }) : null}
-
-          </div>
+              )) : null}
+            </div>
+          )}
         </div>
       </section>
     : null
